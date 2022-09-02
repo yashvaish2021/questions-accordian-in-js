ff878a267ff32e0d70342b76d42c39a50ff7eba5
Yash Vaish <yashvaish2021@gmail.com>
2022-09-02 16:25:38 +0530

    Manipulated dom by using subtree algorithm

---
+++ subtree-query.js
@@ -0,0 +1,16 @@
+const questionBtn = window.document.querySelectorAll(".question-btn");
+const questions = window.document.querySelectorAll(".question");
+
+questionBtn.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    questions.forEach((question) => {
+      if (question.classList.contains("show-text")) {
+        question.classList.remove("show-text");
+      }
+    });
+    const question = e.currentTarget.parentElement.parentElement;
+    question.classList.toggle("show-text");
+  });
+});
+
+console.log("teri maa ki chu");
